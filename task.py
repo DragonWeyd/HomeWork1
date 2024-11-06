bd5e18a9c791e6e368335d660f7fa6459815c4f8
DragonWeyd <romanpopov322@yahoo.com>
2024-11-06 09:18:58 +0300

    06.11

---
+++ task.py
@@ -0,0 +1,18 @@
+#1st program
+
+print(9 ** 0.5 * 5)
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 !=1000.1)
+
+#3rd program
+
+print((2 * 2 + 2) == (2 * ( 2 + 2 )))
+
+#4th program
+
+x = (float('123.456') * 10)  #  перевод c типа данных str на float
+print(int(x % 10))           #  вывод целогочисленного значения
+
+print(int(float('123.456') * 10) % 10)      #   то же самое но в 1 строку
